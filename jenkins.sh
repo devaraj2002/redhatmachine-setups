a93351afb5906121952f999de2b1e7ce1a2a2b4e
devaraj <155232518+devaraj2002@users.noreply.github.com>
2024-09-15 13:23:39 +0530

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,12 @@
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo dnf upgrade -y
+# Add required dependencies for the jenkins package
+sudo dnf install fontconfig java-17-openjdk -y
+sudo dnf install jenkins -y
+sudo systemctl daemon-reload
+
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
