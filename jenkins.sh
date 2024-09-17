a604fe2e8dadd10b14814d5a0b0eddd1af6a859a
devaraj <155232518+devaraj2002@users.noreply.github.com>
2024-09-17 11:52:49 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,12 +1,9 @@
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo yum update -y
+sudo yum install java-17-amazon-corretto -y
+java --version
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo dnf upgrade -y
-# Add required dependencies for the jenkins package
-sudo dnf install fontconfig java-17-openjdk -y
-sudo dnf install jenkins -y
-sudo systemctl daemon-reload
-
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
-sudo systemctl status jenkins
+yum install jenkins -y
+systemctl start jenkins
+systemctl status jenkins
+cat /var/lib/jenkins/secrets/initialAdminPassword
